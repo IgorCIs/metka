6bfe2d5e0b912ba5ab5439ba67df6c2988063eb4
igorCis <igormarga@mail.ru>
2018-09-07 03:14:23 +0300

    added authorize

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 export default {
     port: 1945,
     mongoURL: 'mongodb://localhost:27017/users',
-    redisStore: {
-        url: 'redis://localhost:27017/',
-        secret: 'HUI'
+    redis: {
+        url: 'mongodb://localhost:27017/users',
+        secret: 'udachi ebat'
     }
 }
--- server/routes/admins.routes.js
+++ server/routes/admins.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import * as PostController from './../controllers/admins.controllers'
+import * as PostController from '../controllers/admins.controllers'
 
 const router = new Router()
 
 
-router.route('/admins').get(PostController.login)   
+router.route('/admins/login').get(PostController.login)   
+
+router.route('/admins/logout').get(PostController.logout)   
 
 export default router
